ca136dfaa10d7b590a819fa2ba00b9d0e87c0a9e
Sofia Nguyen <120623425+symsoph@users.noreply.github.com>
2023-04-01 16:29:17 -0700

    got shibe.online to run as a carousel!!!!

--- app/src/main/java/com/example/funapi/MainActivity.kt
+++ app/src/main/java/com/example/funapi/MainActivity.kt
@@ -48,18 +48,62 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    private fun getArtImageURL() {
+        val client = AsyncHttpClient()
+        var objectId = Random.nextInt(from = 37, until = 47899)
+        var clientURL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + objectId
+        client[clientURL, object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Art", "response successful$json")
+                //var resultsJSON = json.jsonObject.getJSONObject(0)
+                //imageURL = resultsJSON.getString("url")
+                imageURL = json.jsonObject.getString("primaryImageSmall")
+                periodDate = json.jsonObject.getString("period")
+                objectTitle = json.jsonObject.getString("title")
+                departmentName = json.jsonObject.getString("department")
+//                if (imageURL.isNullOrBlank()) {
+//                    getArtImageURL()
+//                }
+//                if (periodDate.isBlank()) {
+//                    periodDate = "Unknown"
+//                }
+
+                var i = 9
+                while (i > 5) {
+                    petList.add(imageURL)
+                    --i
+                    getArtImageURL()
+                }
+                val adapter = PetAdapter(petList)
+                rvPets.adapter = adapter
+                rvPets.layoutManager = LinearLayoutManager(this@MainActivity)
+                rvPets.addItemDecoration(DividerItemDecoration(this@MainActivity, LinearLayoutManager.VERTICAL))
+            }
+
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Art Error", errorResponse)
+            }
+        }]
+    }
+
     private fun getDogImageURL() {
         val client = AsyncHttpClient()
         //var picArray = arrayOf<String>("shibes", "birds", "cats")
         //var animal = Random.nextInt(2)
-        //var clientURL = "https://shibe.online/api/" + picArray[animal] + "?count=2&urls=true&httpsUrls=true"
-        client["https://dog.ceo/api/breeds/image/random/20", object : JsonHttpResponseHandler() {
+        //var clientURL = "https://shibe.online/api/" + picArray[animal] + "?count=2&urls=true&httpsUrls=true" --> want https://dog.ceo/api/breeds/image/random/20
+        client["https://shibe.online/api/shibes?count=20&urls=true&httpsUrls=true", object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
                 Log.d("Dog", "response successful$json")
-                val petImageArray = json.jsonObject.getJSONArray("message")
-                for (i in 0 until petImageArray.length()) {
-                    petList.add(petImageArray.getString(i))
-                }
+                val imageArray = json.jsonArray
+                for (i in 0 until 19) {
+                    petList.add(imageArray.getString(i))
+               }
                 val adapter = PetAdapter(petList)
                 rvPets.adapter = adapter
                 rvPets.layoutManager = LinearLayoutManager(this@MainActivity)
